c9f0f93bbeb3a749c50ff056ab2d7b8ea7248c1c
yjhtry <860622588@qq.com>
2024-05-18 21:10:13 +0800

    feat: game demo

---
+++ wasm-vite/src/pages/game.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Universe } from '@wasm/adder'
+
+const game = ref('')
+
+const universe = Universe.new()
+
+function renderLoop() {
+  game.value = universe.render()
+  universe.tick()
+
+  requestAnimationFrame(renderLoop)
+}
+
+onMounted(() => {
+  renderLoop()
+})
+</script>
+
+<template>
+  {{ game }}
+</template>
+
+<style>
+body {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- wasm-vite/typed-router.d.ts
+++ wasm-vite/typed-router.d.ts
@@ -20,6 +20,7 @@ declare module 'vue-router/auto-routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
     '/[...all]': RouteRecordInfo<'/[...all]', '/:all(.*)', { all: ParamValue<true> }, { all: ParamValue<false> }>,
+    '/game': RouteRecordInfo<'/game', '/game', Record<never, never>, Record<never, never>>,
     '/hi/[name]': RouteRecordInfo<'/hi/[name]', '/hi/:name', { name: ParamValue<true> }, { name: ParamValue<false> }>,
   }
 }
